19206e4dabb14132952841ac0f15d2ef8b54b3aa
guitarlomaster <guitarlomaster@gmail.com>
2018-01-12 15:28:30 +0400

    start

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+// подключение сторонних плагинов и библиотек
+
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
+
+
+
+//= partials/sliders-init.js
